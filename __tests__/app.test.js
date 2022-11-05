c60bc8f0d47ec98b2c8c5f880b656df8ef92fe74
Balaji Yaswanth Vankala <balajiyaswanth.v@gmail.com>
2022-11-06 02:04:19 +0530

    added db cross checks and improved tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,14 +11,14 @@ const app = getApp();
 beforeEach(async () => {
     await db.collection('users').insertOne({
         name: 'user1',
-        email: 'user1',
+        email: 'user1@hmail.com',
         password: 'user1',
         followers: [],
         following: []
     });
     await db.collection('users').insertOne({
         name: 'user2',
-        email: 'user2',
+        email: 'user2@hmail.com',
         password: 'user2',
         followers: [],
         following: []
@@ -44,8 +44,8 @@ describe('Test API endpoints', () => {
             const response = await request(app)
                 .post('/api/authenticate')
                 .send({
-                    email: 'abcd',
-                    password: 'abcd'
+                    email: 'user1@hmail.com',     // user1 exists in the database (see beforeEach)
+                    password: 'user1'
                 });
             expect(response.statusCode).toBe(200);
             expect(response.body.message).toBe('Logged in successfully');
@@ -64,7 +64,7 @@ describe('Test API endpoints', () => {
 
     describe('given user is authenticated', () => {
         it('should return profile of authenticated user', async () => {
-            const user = await db.collection('users').findOne({ email: 'user1' });
+            const user = await db.collection('users').findOne({ email: 'user1@hmail.com' });
             const token = createToken(user);
 
             const response = await request(app)
@@ -77,8 +77,8 @@ describe('Test API endpoints', () => {
         });
 
         it('should follow a user', async () => {
-            let user1 = await db.collection('users').findOne({ email: 'user1' });
-            let user2 = await db.collection('users').findOne({ email: 'user2' });
+            const user1 = await db.collection('users').findOne({ email: 'user1@hmail.com' });
+            const user2 = await db.collection('users').findOne({ email: 'user2@hmail.com' });
 
             const user1Id = new mongoose.Types.ObjectId(user1._id).toString();
             const user2Id = new mongoose.Types.ObjectId(user2._id).toString();
@@ -91,38 +91,46 @@ describe('Test API endpoints', () => {
             expect(response.statusCode).toBe(200);
             expect(response.body.message).toBe('You are now following ' + user2.name);
 
-            user1 = await db.collection('users').findOne({ email: 'user1' });
-            user2 = await db.collection('users').findOne({ email: 'user2' });
+            // Check if user1 is following user2
+            const updatedUser1 = await db.collection('users').findOne({ email: 'user1@hmail.com' });
+            const updatedUser2 = await db.collection('users').findOne({ email: 'user2@hmail.com' });
 
-            expect(user2.followers).toContain(user1Id);
-            expect(user1.following).toContain(user2Id);
+            expect(updatedUser2.followers).toContain(user1Id);
+            expect(updatedUser1.following).toContain(user2Id);
         });
 
         it('should unfollow a user', async () => {
-            let user1 = await db.collection('users').findOne({ email: 'user1' });
-            let user2 = await db.collection('users').findOne({ email: 'user2' });
-            
+            const user1 = await db.collection('users').findOne({ email: 'user1@hmail.com' });
+            const user2 = await db.collection('users').findOne({ email: 'user2@hmail.com' });
+
             // update user1 to follow user2
             await db.collection('users').updateOne(
-                { email: 'user1' },
+                { email: 'user1@hmail.com' },
                 { $push: { following: user2._id } }
             );
             await db.collection('users').updateOne(
-                { email: 'user2' },
+                { email: 'user2@hmail.com' },
                 { $push: { followers: user1._id } }
             );
-            user1 = await db.collection('users').findOne({ email: 'user1' });
-            const token = createToken(user1);
+            const currentUser = await db.collection('users').findOne({ email: 'user1@hmail.com' });
+            const token = createToken(currentUser);
             
             const response = await request(app)
                 .post('/api/unfollow/' + user2._id)
                 .set('auth-token', token);
             expect(response.statusCode).toBe(200);
             expect(response.body.message).toBe('You are no longer following ' + user2.name);
+
+            // check if user1 is no longer following user2
+            const updatedUser1 = await db.collection('users').findOne({ email: 'user1@hmail.com' });
+            const updatedUser2 = await db.collection('users').findOne({ email: 'user2@hmail.com' });
+
+            expect(updatedUser2.followers).not.toContainEqual(updatedUser1._id);
+            expect(updatedUser1.following).not.toContainEqual(updatedUser2._id);
         });
 
         it('should create a post', async () => {
-            const user = await db.collection('users').findOne({ email: 'user1' });
+            const user = await db.collection('users').findOne({ email: 'user1@hmail.com' });
             const token = createToken(user);
 
             const response = await request(app)
@@ -135,13 +143,14 @@ describe('Test API endpoints', () => {
             expect(response.statusCode).toBe(200);
             expect(response.body.title).toBe('This is a test post');
             expect(response.body.desc).toBe('This is a test post description');
-
+            
+            // check if post is created in database
             const post = await db.collection('posts').findOne({ title: 'This is a test post' });
             expect(post).toBeDefined();
         });
 
         it('should delete a post', async () => {
-            const user = await db.collection('users').findOne({ email: 'user1' });
+            const user = await db.collection('users').findOne({ email: 'user1@hmail.com' });
             const token = createToken(user);
             await db.collection('posts').insertOne({
                 title: 'This is a test post',
@@ -158,12 +167,12 @@ describe('Test API endpoints', () => {
             expect(response.statusCode).toBe(200);
             expect(response.body.message).toBe('Post deleted successfully');
 
-            let oldPost = await db.collection('posts').findOne({ title: 'This is a test post' });
-            expect(oldPost).toBeNull(); // post should be deleted
+            const deletedPost = await db.collection('posts').findOne({ title: 'This is a test post' });
+            expect(deletedPost).toBeNull(); // post should be deleted
         });
 
         it('should like a post', async () => {
-            const user = await db.collection('users').findOne({ email: 'user1' });
+            const user = await db.collection('users').findOne({ email: 'user1@hmail.com' });
             const token = createToken(user);
             await db.collection('posts').insertOne({
                 title: 'This is a test post',
@@ -173,6 +182,9 @@ describe('Test API endpoints', () => {
                 likes: [],
                 comments: []
             });
+
+            const user1Id = new mongoose.Types.ObjectId(user._id).toString();
+
             const post = await db.collection('posts').findOne({ title: 'This is a test post' });
             const response = await request(app)
                 .post('/api/like/' + post._id)
@@ -180,12 +192,13 @@ describe('Test API endpoints', () => {
             expect(response.statusCode).toBe(200);
             expect(response.body.message).toBe('Liked successfully');
 
-            // const updatedPost = await db.collection('posts').findOne({ title: 'This is a test post' });
-            // expect(updatedPost.likes[0]).toEqual(user._id);
+            // Check if post is liked by user1
+            const updatedPost = await db.collection('posts').findOne({ title: 'This is a test post' });
+            expect(updatedPost.likes).toContain(user1Id);
         });
 
         it('should unlike a post', async () => {
-            const user = await db.collection('users').findOne({ email: 'user1' });
+            const user = await db.collection('users').findOne({ email: 'user1@hmail.com' });
             const token = createToken(user);
             await db.collection('posts').insertOne({
                 title: 'This is a test post',
@@ -196,16 +209,23 @@ describe('Test API endpoints', () => {
                 comments: []
             });
             const post = await db.collection('posts').findOne({ title: 'This is a test post' });
+
             const response = await request(app)
                 .post('/api/unlike/' + post._id)
                 .set('auth-token', token);
             expect(response.statusCode).toBe(200);
             expect(response.body.message).toBe('Unliked successfully');
+            expect(response.body.likes).toEqual(0);
+
+            // Check if user is no longer in likes array
+            const updatedPost = await db.collection('posts').findOne({ title: 'This is a test post' });
+            expect(updatedPost.likes).not.toContain(user._id);
         });
 
         it('should comment on a post', async () => {
-            const user = await db.collection('users').findOne({ email: 'user1' });
+            const user = await db.collection('users').findOne({ email: 'user1@hmail.com' });
             const token = createToken(user);
+            // Initialize a post
             await db.collection('posts').insertOne({
                 title: 'This is a test post',
                 desc: 'This is a test post description',
@@ -222,13 +242,17 @@ describe('Test API endpoints', () => {
                     desc: 'This is a test comment'
                 });
             expect(response.statusCode).toBe(200);
+            
+            // DB check
+            const updatedPost = await db.collection('posts').findOne({ title: 'This is a test post' });
+            expect(updatedPost.comments[0].desc).toBe('This is a test comment');
         });
     });
 });
 
 afterEach(async () => {
-    await db.collection('users').deleteOne({ email: 'user1' });
-    await db.collection('users').deleteOne({ email: 'user2' });
+    await db.collection('users').deleteOne({ email: 'user1@hmail.com' });
+    await db.collection('users').deleteOne({ email: 'user2@hmail.com' });
     await db.collection('posts').deleteOne({ title: 'This is a test post' });
 });
 
