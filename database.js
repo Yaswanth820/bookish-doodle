fcc0212dea70135d8385d3c903458e59e9711eeb
Balaji Yaswanth Vankala <balajiyaswanth.v@gmail.com>
2022-11-20 22:23:03 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
 
 module.exports.setupDB = () => {
-    // Cloud DB
-    // mongodb+srv://user:user123@cluster1.gle5k.mongodb.net/social-media-app
-    
     // Local DB
-    // mongodb://mongo:27017/social-media-app
-    mongoose.connect('mongodb+srv://user:user123@cluster1.gle5k.mongodb.net/social-media-app');
+    mongoose.connect('mongodb://mongo:27017/social-media-app');
     const db = mongoose.connection;
     db.once('open', () => {
         console.log('Connected to MongoDB');
